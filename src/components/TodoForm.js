79523a6629046db0d43032116eb8098d8fadca16
Lingling Gan <linglinggan123@gmail.com>
2023-07-05 11:38:52 -0700

    Add TodoForm component

---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import Input from "@mui/joy/Input";
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/joy/Button";
+import { useState } from "react";
+export const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState("");
+  const handleChange = (evt) => {
+    setText(evt.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+  return (
+    <ListItem>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Type in here…"
+          variant="outlined"
+          color="primary"
+          onChange={handleChange}
+          value={text}
+          endDecorator={
+            <Button
+              variant="solid"
+              color="primary"
+              type="submit"
+              sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}>
+              Add
+            </Button>
+          }
+        />
+      </form>
+    </ListItem>
+  );
+};
